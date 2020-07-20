0e31648e6b7056cbe2a07228bbb92bcef0266f1b
kyln7 <41321737+kyln7@users.noreply.github.com>
2020-07-20 17:54:23 +0800

    update 2020/7/20

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEngine.UI;
 using UnityEngine;
 using TRpgMap;
 
@@ -8,6 +9,7 @@ public class Testing : MonoBehaviour
 {
     private string mapDataPath;
     private GridArray mapData;
+    public Text T;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,17 @@ public class Testing : MonoBehaviour
         {
             Debug.Log(grid.ToString());
         }
+        Dialogue dialogue = SaveSystem.LoadDialogue("Test","test");
+        foreach(string text in dialogue.sentences)
+        {
+            Debug.Log(text);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        T.text = TimeSystem.GetTime();
         if (Input.GetMouseButtonDown(0))
         {
             GameObject obj = GameSystem.GetGameObjectByMouse("Interactable");
---
+++ Assets/Scripts/TimeSystem/TimeSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSystem : MonoBehaviour
+{
+    [SerializeField]
+    private int gameTime_Hour = 4;
+    private static float gameTime;
+
+    public Event OnTimeTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameTime = gameTime_Hour * 60 * 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime -= Time.deltaTime;
+    }
+
+    public static string GetTime()
+    {
+        return (int)(gameTime / 3600) + ":" + (int)((gameTime / 60) % 60) + "." + (int)(gameTime % 60);
+    }
+}
