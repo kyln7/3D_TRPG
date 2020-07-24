b3b00286d9e65b9c3b207a132e0267c09b67e03e
dutianyu1234 <1615171019@qq.com>
2020-07-24 10:06:40 +0800

    123

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
-{ 
-    private List<string> sentences;
+{
+    public GameObject DialogBox;
+
+    Dialogue dialogue;
+    int i = 0;
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new List<string>();
+        dialogue = new Dialogue();
+        dialogue.LoadDialogue("01");
+        Debug.Log(dialogue.sentences.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (i > dialogue.sentences.Count - 1) i = 0;
+
+            Debug.Log(dialogue.sentences[i].getContent());
+            DialogBox.transform.GetChild(0).GetComponentInChildren<Text>().text = dialogue.sentences[i].getName();
+            DialogBox.transform.GetChild(1).GetComponentInChildren<Text>().text = dialogue.sentences[i].getContent();
+            i += 1;
+        }
     }
+
 }
