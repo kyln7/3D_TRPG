60c780aa0f953c26079ed020b7f529d31695ddc2
kyln7 <41321737+kyln7@users.noreply.github.com>
2020-07-27 17:41:56 +0800

    update AI

---
+++ Assets/Scripts/AI/InformationPool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TrpgAI
+{
+    public class InformationPool
+    {
+        private static InformationPool instance = new InformationPool();
+        private InformationPool() { }
+        public static InformationPool GetInstance()
+        {
+            return instance;
+        }
+        public void AddInformation(Information information)
+        {
+            //todo
+        }
+    }
+
+    public abstract class Information{
+        //todo
+    }
+
+}
+
